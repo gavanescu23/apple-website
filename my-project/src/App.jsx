53d2b0c61863b386dc51d52876c573acd3181a7b
Vlad Gavanescu <Vlad.Gavanescu@dynatacorp.com>
2024-10-23 18:23:11 +0300

    Initial commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import NavBar from "./components/NavBar"
+
+function App() {
+
+  return (
+    <main className="bg-black">
+      <NavBar/>
+      <Hero/>
+      <Highlights/>
+    </main>
+  )
+}
+
+export default App
